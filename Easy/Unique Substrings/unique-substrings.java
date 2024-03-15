3790bb686b8cbb819c8d4669409db5757f44809a
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2024-03-15 10:55:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Unique Substrings/unique-substrings.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.util.ListIterator;
+class GFG {
+	public static void main (String[] args) {
+		
+		//Taking input using class Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Taking count of total number of testcases
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		  
+		  
+		  //Taking the string as input
+		  String str=sc.nextLine();
+		  Solution ob = new Solution();
+		  //Calling the unique_substring method
+		  //and printing the result
+		  System.out.println(ob.unique_substring(str));
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+
+        public static int unique_substring(String str)
+    {
+        //Your code here
+        Set<String> uniqueSubstrings = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                uniqueSubstrings.add(str.substring(i, j));
+            }
+        }
+        return uniqueSubstrings.size();
+    }
+}
