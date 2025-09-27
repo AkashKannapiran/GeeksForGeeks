d3dc0ed6a3283f79f00a187a0b4ed5f20b0e8ddc
Akash Kannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-09-27 20:14:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    int noOfRound = 0;
+    
+    public int towerOfHanoi(int n, int from, int to, int aux) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        towerOfHanoi(n - 1, from, aux, to);
+        noOfRound++;
+        towerOfHanoi(n - 1, aux, to, from);
+        
+        return noOfRound;
+        
+    }
+}
